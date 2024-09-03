1d91d5ae943af20d0f5fe784ec823d59e1379a5b
richarlisonn <richarlison.esc@gmail.com>
2024-09-03 17:38:27 -0300

    Aula de Logica, aula sobre Vetores

---
+++ Logica de Pro/AULA/main.py
@@ -0,0 +1,5 @@
+celebridades = ["Tiaguinho", "Isa", "Fernando de Sorocaba", "Jorge e Mateus"]
+print("Celebridades de Hoje Serão:",(celebridades))
+
+sub= celebridades[celebridades.index("Jorge e Mateus")] = "Leonardo"
+print ("A celebridade Jorge e Mateus, não irá conseguir ir ao show", (sub) , "irá substituir" )
